3b2e6bddeaa02d3dcb535d843380fc44423ba099
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-11-11 00:15:34 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+// а ты возьми и добавь нолик в начале стоки если там не два символа
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+// а ты возьми и давай считай время да
+function getTimeComponents(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
+
+function momentTime(time) {
+  return getTimeComponents(time);
+}
+
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.targetDate = targetDate;
     this.refs = {
-      $days: document.querySelector(`${selector} [data-value='days']`),
-      $hourse: document.querySelector(`${selector} [data-value='hours']`),
-      $mins: document.querySelector(`${selector} [data-value='mins']`),
-      $secs: document.querySelector(`${selector} [data-value='secs']`),
+      $days: document.querySelector(`${selector} [data-value="days"]`),
+      $hours: document.querySelector(`${selector} [data-value="hours"]`),
+      $mins: document.querySelector(`${selector} [data-value="mins"]`),
+      $secs: document.querySelector(`${selector} [data-value="secs"]`),
     };
     this.timerId = null;
   }
@@ -17,32 +38,12 @@ class CountdownTimer {
     }, 1000);
   }
   updateInterface({ days, hours, mins, secs }) {
-    const { $days, $hourse, $mins, $secs } = this.refs;
+    const { $days, $hours, $mins, $secs } = this.refs;
     $days.textContent = days;
-    $hourse.textContent = hours;
+    $hours.textContent = hours;
     $mins.textContent = mins;
     $secs.textContent = secs;
   }
-}
-// а ты возьми и давай считай время да
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return days, hours, mins, secs;
-}
-
-// а ты возьми и добавь нолик в начале стоки если там не два символа
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
-
-function momentTime(time) {
-  return getTimeComponents(time);
 }
 
 function initTimer(selector, targetDate) {
@@ -50,4 +51,8 @@ function initTimer(selector, targetDate) {
   backTimer.startTimer();
 }
 
-initTimer("timer-1", new Date("Jul 17, 2022"));
+initTimer("#timer-1", new Date("Jul 17, 2022"));
+
+// //отсебятина
+// const inputLabel = document.querySelector(".countdown-label");
+// console.log(inputLabel);
